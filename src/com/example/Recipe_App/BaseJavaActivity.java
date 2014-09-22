cbb5074732d25f8c68df5cd0340c5aebe3b83daa
wags906 <alexpreston117@gmail.com>
2014-09-21 17:37:34 -0700

    adding a third java activity

---
+++ src/com/example/Recipe_App/BaseJavaActivity.java
@@ -0,0 +1,10 @@
+package com.example.Recipe_App;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Wags on 9/21/14.
+ */
+public class BaseJavaActivity extends Activity {
+}
---
+++ src/com/example/Recipe_App/ThirdActvitiy.java
@@ -0,0 +1,16 @@
+package com.example.Recipe_App;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Wags on 9/21/14.
+ */
+
+public class ThirdActvitiy extends BaseJavaActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_activity);
+    }
+}
